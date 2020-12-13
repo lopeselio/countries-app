44292cc966811b60380d776f425593eaf0f8a089
lopeselio <lopeselio@gmail.com>
2020-12-14 01:29:09 +0530

    packages

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { Button } from 'react-native-elements';
+
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
@@ -10,11 +12,33 @@ import {
 // import Countries from "./screens/country"
 import Api from "./components/api2"
 
+// import { createStackNavigator } from '@react-navigation/stack';
+
+// // const Stack = createStackNavigator();
+
+// const myOptions = {
+//   title:"My Home",
+//   headerTintColor:"white",
+//   headerStyle:{
+//     backgroundColor:"#006aff"
+//   }
+// }
+
+
+// function MyStack() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Home" component={Feed} options={myOptions} />
+//       {/* <Stack.Screen name="Notifications" component={Notifications} /> */}
+//     </Stack.Navigator>
+//   );
+// }
+
 function Feed({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title="Open" onPress={() => navigation.openDrawer()} />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#00FFFF' }}>
+      {/* <Text>Home Screen</Text> */}
+      <Button  type="outline" title="Open" onPress={() => navigation.openDrawer()} />
     </View>
   );
 }
@@ -25,6 +49,7 @@ function CustomDrawerContent(props) {
     <DrawerContentScrollView {...props}>
       <DrawerItemList {...props} />
       <DrawerItem
+        
         label="Close drawer"
         onPress={() => props.navigation.closeDrawer()}
       />
@@ -38,7 +63,7 @@ function MyDrawer() {
   return (
     <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
       <Drawer.Screen name="Home" component={Feed} />
-      <Drawer.Screen name="Countries" component={Api} />
+      <Drawer.Screen name="Regions" component={Api} />
     </Drawer.Navigator>
   );
 }
@@ -46,6 +71,7 @@ function MyDrawer() {
 export default function App() {
   return (
     <NavigationContainer>
+      {/* <MyStack /> */}
       <MyDrawer />
     </NavigationContainer>
   );
